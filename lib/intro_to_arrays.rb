5081fce674badff570e0ee6f894f351797f41e30
Stephanie Purple <spurple133@gmail.com>
2019-03-07 18:36:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,7 +6,6 @@ def array_with_two_elements
   array=["flippy","bella"]
 end
 
-def first_element
-  my_first_element=["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_first_element[0]
+def first_element(array)
+  array.first
 end
