40ad5ff49c084f5ac29d8d3a6586fc0f6dbcaf6d
Stephanie Purple <spurple133@gmail.com>
2019-03-07 18:30:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,8 @@ end
 def array_with_two_elements
   array=["flippy","bella"]
 end
+
+def first_element
+  array=["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array.index(0)
+end
